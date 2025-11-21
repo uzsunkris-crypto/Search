35a48deb1aa532949cad69fdeafee42d5d126333
uzsunkris-crypto <smilealexsunday@gmail.com>
2025-11-21 15:05:29 +0100

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,6 @@
+self.addEventListener('install', e => { self.skipWaiting(); });
+self.addEventListener('activate', e => { self.clients.claim(); });
+
+self.addEventListener('fetch', event => {
+  event.respondWith(fetch(event.request).catch(()=> caches.match(event.request)));
+});

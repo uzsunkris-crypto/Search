c9a79268fbf8b728615830b2d04842b93ee91f0d
uzsunkris-crypto <smilealexsunday@gmail.com>
2025-11-21 15:02:35 +0100

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,16 @@
+// firebase-config.js
+// Replace window.FIREBASE_CLIENT_CONFIG = null with your Firebase client config from the console.
+
+window.FIREBASE_CLIENT_CONFIG = null;
+
+/*
+Example:
+window.FIREBASE_CLIENT_CONFIG = {
+  apiKey: "AIza....",
+  authDomain: "project-id.firebaseapp.com",
+  projectId: "project-id",
+  storageBucket: "project-id.appspot.com",
+  messagingSenderId: "1234567890",
+  appId: "1:123456789:web:abcd..."
+};
+*/

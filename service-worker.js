c2578292b4c19b79c68e572fb6a23034369ef1a1
uzsunkris-crypto <smilealexsunday@gmail.com>
2025-12-29 02:06:09 +0100

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,6 +0,0 @@
-self.addEventListener('install', e => { self.skipWaiting(); });
-self.addEventListener('activate', e => { self.clients.claim(); });
-
-self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request).catch(()=> caches.match(event.request)));
-});
